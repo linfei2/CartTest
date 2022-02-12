84d26f38f895925b96b2a7031353830c2a2500cd
linfei2 <dominika.matczynska@gmail.com>
2022-02-12 15:51:39 +0100

    Updated scrollig script so that it doesn't contain hard coded coordinates

--- BaseClass.py
+++ BaseClass.py
@@ -4,17 +4,20 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.mark.usefixtures('init_driver')
-class BaseTest:
+class BaseClass:
+    def find(self, element):
+        return WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(element))
+
     def get_url(self, url):
         self.driver.get(url)
 
     def click_element(self, element):
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(element)).click()
 
-    def execute(self, script):
-        self.driver.execute_script(script)
+    def execute(self, *args):
+        self.driver.execute_script(*args)
 
-    def get_inner_text(self, element):
+    def get_value(self, element):
         el = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(element))
         text = el.get_attribute('innerText')
         return text
--- test_cart.py
+++ test_cart.py
@@ -1,26 +1,25 @@
 import pytest
-from BaseTest import BaseTest
+from BaseClass import BaseClass
 from time import sleep
 from locators import Loc
 
 
-class TestCart(BaseTest):
+class TestCart(BaseClass):
 
     @pytest.fixture()
     def add_to_cart(self):
         self.get_url('https://moim.store/')
         self.click_element(Loc.NEW_IN_BTN)
         self.click_element(Loc.FIRST_PRODUCT)
-        self.execute('window.scrollTo(0, 400)')
+        self.execute('arguments[0].scrollIntoView();', self.find(Loc.ADD_TO_CART_BTN))
         sleep(1)
         self.click_element(Loc.ADD_TO_CART_BTN)
 
     def test_add_to_cart(self, add_to_cart):
-        sleep(1)
-        count = self.get_inner_text(Loc.CART_ICON)
+        count = self.get_value(Loc.CART_ICON)
         assert count == '1'
 
     def test_remove_from_cart(self, add_to_cart):
         self.click_element(Loc.REMOVE_BTN)
-        message = self.get_inner_text(Loc.EMPTY_CART_NOTIFICATION)
+        message = self.get_value(Loc.EMPTY_CART_NOTIFICATION)
         assert message == "Twój koszyk aktualnie jest pusty."
